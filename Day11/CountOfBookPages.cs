2d72723a7348adbd1635964b0d410b658760a80a
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-23 18:00:47 +0530

    Add : Count Book Pages

---
+++ Day11/CountOfBookPages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public class CountOfBookPages
+    {
+        public int GetRemainingChapters(int N, int[,] chapters, int K)
+        {
+            int remainingChapters = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int startPage = chapters[i, 0];
+                int endPage = chapters[i, 1];
+
+                if (startPage <= K && K <= endPage)
+                {
+                    remainingChapters = N - i;
+                    break;
+                }
+            }
+
+            return remainingChapters;
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day11
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter the number of chapters in the book:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[,] chapters = new int[N, 2];
+
+            Console.WriteLine("Enter the start and end pages of each chapter:");
+            for (int i = 0; i < N; i++)
+            {
+                string[] pages = Console.ReadLine().Split(' ');
+                chapters[i, 0] = Convert.ToInt32(pages[0]);
+                chapters[i, 1] = Convert.ToInt32(pages[1]);
+            }
+
+            Console.WriteLine("Enter the current reading page:");
+            int K = Convert.ToInt32(Console.ReadLine());
+
+            //int remainingChapters = GetRemainingChapters(N, chapters, K);
+            Console.WriteLine("Number of chapters yet to read: " + GetRemainingChapters(N, chapters, K));
+        }
+    }
+}
